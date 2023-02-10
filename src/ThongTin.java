86b0f1009a8a75268536785fc5ea7b8f46ff0753
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2023-02-10 21:51:48 +0700

    JP1-FX

--- src/ThongTin.java
+++ src/ThongTin.java
@@ -1,29 +1,34 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class ThongTin {
+public class ThongTin implements Initializable {
 
     public TextField txtFullName;
     public TextField txtEmail;
+    public static TextField stFullName;
+    public static TextField stEmail;
     public Text txtInfo;
-
     public ObservableList<Student> listStudent = FXCollections.observableArrayList();
-    public ListView<Student> lv;
-
-    public Student editStudent;
+    public static Student editStudent;
+    public TableView<Student> tbview;
+    public TableColumn<Student, String> cFullname;
+    public TableColumn<Student, String> cEmail;
+    public TableColumn<Student, Button> cAction;
 
     public void submit(ActionEvent actionEvent) {
         String fn = txtFullName.getText();
         String em = txtEmail.getText();
-
-        if(editStudent == null) {
+        if (editStudent == null) {
 //        System.out.println("full name :" +fn);
 //        System.out.println("Email : " +em);
 //        String txt = txtInfo.getText();
@@ -34,18 +39,18 @@ public class ThongTin {
             Student s = new Student(fn, em);
             listStudent.add(s);
 
-        }else{
+        } else {
 //            editStudent.setFullName(fn);
 //            editStudent.setEmail(em);
-            for(Student s: listStudent){
-                if(s.email.equals(editStudent.email) && s.fullName.equals(editStudent.fullName)){
+            for (Student s : listStudent) {
+                if (s.email.equals(editStudent.email) && s.fullName.equals(editStudent.fullName)) {
                     s.setFullName(fn);
                     s.setEmail(em);
                 }
             }
         }
-        lv.setItems(listStudent);
-        lv.refresh();
+        tbview.setItems(listStudent);
+        tbview.refresh();
         editStudent = null;
         clearInput();
 
@@ -70,14 +75,24 @@ public class ThongTin {
 //        });
     }
 
-    void  clearInput(){
+    void clearInput() {
         txtEmail.clear();
         txtFullName.clear();
     }
 
     public void edit(MouseEvent mouseEvent) {
-        editStudent = lv.getSelectionModel().getSelectedItem();
+//        editStudent = lv.getSelectionModel().getSelectedItem();
         txtFullName.setText(editStudent.getFullName());
         txtEmail.setText(editStudent.getEmail());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cFullname.setCellValueFactory(new PropertyValueFactory<>("fullName"));
+        cEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        cAction.setCellValueFactory(new PropertyValueFactory<>("edit"));
+
+        stFullName = txtFullName;
+        stEmail = txtEmail;
     }
 }
